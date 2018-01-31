28595c0c6ade476a152978e5ac77e83f13ef7db4
Yakul <xayide.rg@gmail.com>
2018-01-31 22:18:33 +0100

    Refixing the menu

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # TODO:
-# 1. Añadir el menu con botones
 # 2. Añadir la opcion de enviar un feedback o de informar de un link que ya no sirve 
 # 3. Añadir la opcion de enviar los PDFs (o links para descargar esos PDFs para evitar
 #   problemas)
@@ -10,8 +9,9 @@
 # 5. Añadir los putos subredditsy todos los links que quedan
 # 6. ¿Nueva categoria rollo hackgames (e.g, hackthissite)?
 
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 import utilities as utils
 import msg
@@ -21,6 +21,24 @@ token = utils.readToken()
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+
+def menu(bot, update):
+    """ Creates the menu. """
+    keyboard = [
+        [InlineKeyboardButton("Pages",    callback_data='Pages'),
+         InlineKeyboardButton("Books",    callback_data='Books')],
+        [InlineKeyboardButton("Forums",   callback_data='Forums'),
+         InlineKeyboardButton("Samples",  callback_data='MalwareSamples')],
+        [InlineKeyboardButton("Tools",    callback_data='ProjectsAndTools'),
+         InlineKeyboardButton("RSSFeeds", callback_data='RSSFeeds')],
+        [InlineKeyboardButton("Sites",    callback_data='SiteLists'),
+         InlineKeyboardButton("Twitter",  callback_data='TwitterLists')]
+    ]
+
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text("What are you looking for?", reply_markup=reply_markup)
 
 
 
@@ -111,6 +129,60 @@ def sendBooks(bot, update):
     bot.send_message(chat_id=s.chat_id, text=utils.getLinks('Books'))
 
 
+
+
+
+###############
+#   Botones   #
+###############
+
+def buttons_handler(bot, update):
+    """ Crea el menu de botones. """
+
+    query    = update.callback_query
+
+    message  = query.message.message_id
+    petition = query.data
+    chat_id  = query['message']['chat']['id']
+    user     = query['message']['chat']['firts_name']
+    
+    print("\tUsuario: " + user + " [" + chat_id + "]")
+    print("\tOpcion: " + petition)
+
+
+
+"""
+    bot.edit_message_text(text=utils.getLinks(petition),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+"""
+
+
+def unknown(bot, update):
+    """ Recibe cualquier comando no reconocido e informa de que no existe. """
+
+    s = update.message
+    
+    echo(bot, update)
+    bot.send_message(chat_id=s.chat_id, text=msg.infoUnkown)
+    print(msg.unknown)
+
+
+def information(bot, update):
+    """ Recibe un mensaje cualquiera (no comando) y manda instrucciones
+        para el bot. """
+
+    s = update.message
+    echo(bot, update)
+    bot.send_message(chat_id=s.chat_id, text=msg.infoInformation)
+
+
+def reportDown(bot, update):
+    pass
+
+
+
+
 def Main():
 
     print(msg.infoInit)
@@ -118,6 +190,8 @@ def Main():
     updater = Updater(token = token)
     dp = updater.dispatcher
 
+
+    dp.add_handler(MessageHandler(Filters.text, information))
     dp.add_handler(CommandHandler('pages', sendPages))
     dp.add_handler(CommandHandler('rss', sendRSSFeeds))
     dp.add_handler(CommandHandler('samples', sendSamples))
@@ -128,6 +202,14 @@ def Main():
     dp.add_handler(CommandHandler('books', sendBooks))
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
+
+
+    dp.add_handler(CommandHandler('menu', menu))
+    dp.add_handler(CallbackQueryHandler(buttons_handler))
+
+    dp.add_handler(MessageHandler(Filters.command, unknown))
+
+    
 
 
     updater.start_polling()
